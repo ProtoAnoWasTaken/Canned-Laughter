e3b36f8a6618a6960c835abbcb92734c2dfc59c6
ProtoAno <steamzm01@gmail.com>
2026-07-17 16:36:54 -0400

    exp works, the muse off-by-one are you kidding me

--- items/blind/boss_muse.lua
+++ items/blind/boss_muse.lua
@@ -8,7 +8,7 @@ CL.register_standard_boss({
     mult = 2,
     loc_txt = { name = "The Muse", text = { "Poker hands you haven't", "played do not score" } },
     debuff_hand = function(self, cards, hand, handname)
-        local triggered = (G.GAME.hands[handname].played or 0) <= 1
+        local triggered = (G.GAME.hands[handname].played or 0) == 0
         if G and G.GAME and G.GAME.blind then G.GAME.blind.triggered = triggered end
         return triggered
     end,
