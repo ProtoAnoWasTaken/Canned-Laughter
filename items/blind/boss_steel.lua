9c349d1072cab1719eb68e4aba2189979c10011c
ProtoAno <steamzm01@gmail.com>
2026-07-18 20:28:33 -0400

    The Steel fix

--- items/blind/boss_steel.lua
+++ items/blind/boss_steel.lua
@@ -9,9 +9,10 @@ CL.register_standard_boss({
     mult = 2,
     loc_txt = { name = "The Steel", text = { "Poker hands use their", "first-level Chips and Mult" } },
     modify_hand = function(self, cards, poker_hands, text, mult, chips)
-        local handname = next(poker_hands or {})
-        local hand = handname and G.GAME.hands[handname]
-        if not hand then return mult, chips, false end
-        return hand.s_mult + hand.l_mult, hand.s_chips + hand.l_chips, true
+        local hand = text and G.GAME.hands[text]
+        if not hand then
+            return mult, chips, false
+        end
+        return hand.s_mult, hand.s_chips, true
     end,
 })
